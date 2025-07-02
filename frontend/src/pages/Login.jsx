9085aeef8876516375e935d85e7c00196e63170e
RunyararoMucheche <your_email@example.com>
2025-07-02 17:53:27 -0400

    Create Login and Sign Up forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
+
+  const onSubmitHandler = async (evt) => {
+    evt.preventDefault();
+  };
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
@@ -49,7 +56,9 @@ const Login = () => {
           </p>
         )}
       </div>
-      <button></button>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
     </form>
   );
 };
