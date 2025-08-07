f6fe28242fe667cbe58d474846657deb93879dbb
RunyararoMucheche <your_email@example.com>
2025-08-06 20:45:31 -0400

    Add Cart data from backend and connect it to frontend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // App Config
 const app = express();
@@ -17,11 +18,13 @@ app.use(express.json());
 app.use(cors());
 
 // API Endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
-app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
 
 app.listen(port, () => {
   console.log(`SERVER LISTENING ON PORT: ${port}`);
